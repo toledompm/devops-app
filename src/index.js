5debbedd0490e640bcb80f8bec2527c271d0a3bf
jasondavindev <jasondavin.dev@gmail.com>
2021-04-05 22:24:22 -0300

    chore: export modules

--- src/index.js
+++ src/index.js
@@ -1,26 +1,12 @@
 const express = require('express')
-const assert = require('assert')
-
-class Task {
-  constructor(params) {
-    assert.ok(params.id)
-    this.id = params.id
-
-    this.description = params?.description || ''
-    this.title = params?.title || ''
-    this.status = 'ACTIVE'
-  }
-
-  updateStatus(status) {
-    this.status = status
-  }
-}
+const Task = require('./task')
 
 const tasks = []
 let taskIdCounter = 1
 
 const { PORT } = process.env
 const app = express()
+
 app.use(express.json())
 
 app.get('/', (_, res) => res.json({ status: 'ok' }))
@@ -88,3 +74,5 @@ app.put('/task/:id/update', (req, res) => {
 })
 
 app.listen(PORT, () => console.log('Server listening on port', PORT))
+
+module.exports = app
---
+++ src/task.js
@@ -0,0 +1,16 @@
+class Task {
+  constructor(params) {
+    assert.ok(params.id)
+    this.id = params.id
+
+    this.description = params?.description || ''
+    this.title = params?.title || ''
+    this.status = 'ACTIVE'
+  }
+
+  updateStatus(status) {
+    this.status = status
+  }
+}
+
+module.exports = Task
