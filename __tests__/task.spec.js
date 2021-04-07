b99fae15ae563ad1110112aa448fad4516ef9a65
Jason Carneiro <jasondavin.dev@gmail.com>
2021-04-07 20:59:07 -0300

    Feat - init unit tests (#3)
    * feat: init unit tests
    
    * Update __tests__/task.spec.js
    
    Co-authored-by: toledompm <45714268+toledompm@users.noreply.github.com>
    
    Co-authored-by: toledompm <45714268+toledompm@users.noreply.github.com>

---
+++ __tests__/task.spec.js
@@ -0,0 +1,47 @@
+const Task = require('../src/task')
+
+describe('Task', () => {
+  describe('#constructor', () => {
+    describe('when all params are valids', () => {
+      it('should returns success', () => {
+        const params = {
+          id: 1,
+          description: 'Install the windows',
+          title: 'Destroy your pc',
+        }
+
+        const task = new Task(params)
+
+        expect(task.id).toEqual(1)
+        expect(task.description).toEqual(params.description)
+        expect(task.title).toEqual(params.title)
+        expect(task.status).toEqual('ACTIVE')
+      })
+    })
+
+    describe('when the id is invalid', () => {
+      it('should throws an error', () => {
+        const params = {
+          id: null,
+        }
+
+        expect(() => new Task(params)).toThrowError()
+      })
+    })
+  })
+
+  describe('#updateStatus', () => {
+    it('should updates the task status', () => {
+      const params = {
+        id: 1,
+        description: 'Task A',
+        title: 'Task A',
+      }
+
+      const myTask = new Task(params)
+      myTask.updateStatus('DONE')
+
+      expect(myTask).toMatchObject({ status: 'DONE' })
+    })
+  })
+})
